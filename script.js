8500d8be9b7ce8e3069c02fa7fe4b0ee7d041a63
Sulley-Sadick <sulleysadick766@gmail.com>
2025-06-25 21:55:47 +0000

    Changed General styles and Javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+// activate strict mode
+"use strict";
+
+// select elements
+const pageViews = document.getElementById("page-views");
+const rangeSlider = document.getElementById("range-input");
+const billingAmount = document.getElementById("billing-amount");
+const toggleBtn = document.getElementById("switch");
+console.log(pageViews, rangeSlider, billingAmount, toggleBtn);
