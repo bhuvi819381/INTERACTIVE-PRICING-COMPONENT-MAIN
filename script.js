99421708322196ebbd84f01ee7fb77dd5125172b
karan819381 <karan819381@gmail.com>
2025-06-26 11:43:00 +0530

    remove node_modules

--- script.js
+++ script.js
@@ -6,4 +6,4 @@ const pageViews = document.getElementById("page-views");
 const rangeSlider = document.getElementById("range-input");
 const billingAmount = document.getElementById("billing-amount");
 const toggleBtn = document.getElementById("switch");
-console.log(pageViews, rangeSlider, billingAmount, toggleBtn);
+//console.log(pageViews, rangeSlider, billingAmount, toggleBtn);
